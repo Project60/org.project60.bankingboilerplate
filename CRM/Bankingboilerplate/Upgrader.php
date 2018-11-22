3119cdeb1370e1c484911f426cd51a1bcf865ffd
B. Endres <endres@systopia.de>
2018-11-22 13:57:32 +0100

    added upgrader

---
+++ CRM/Bankingboilerplate/Upgrader.php
@@ -0,0 +1,16 @@
+<?php
+use CRM_Bankingboilerplate_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Bankingboilerplate_Upgrader extends CRM_Bankingboilerplate_Upgrader_Base {
+
+  /**
+   * Make sure our module's in the list
+   */
+  public function enable() {
+    // TODO
+  }
+  
+}
